418f14b6d167d4c4c6671136a7e0130d1bc97ca0
DonnieG3 <donniegardner@users.noreply.github.com>
2026-07-15 22:59:38 -0400

    Add Bodacious Bazaar site files

---
+++ public_html/index-old.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta content="The Bodacious Bazaar is a 3 day shopping extravaganza and festival held twice a year,in the Spring and Fall, located in Hampton, Virginia. We are a family owned and operated business and community minded. Our goal is to bring a quality show to the Hampton Roads Area and to donate a portion of the proceeds to chosen local nonprofits." name="Description" />
+	<meta content="Bodacious Bazaar, Shopping, Fall, Bazaar, Shopping, Spring, Gifts, Hampton Roads, Festival" name="Keywords" />
+<title>Bodacious Bazaar</title>
+
+
+</head>
+
+<body onload="window.location = 'https://bodaciousbazaar.com/fall/'">
+<center>
+<img class="logo" itemprop="image" src="logo.png" alt="Bodacious Bazaar" title="Bodacious Bazaar" /><br />
+<div style="text-decoration:none; color:#333; font-family:Georgia, 'Times New Roman', Times, serif; font-size:16px;">
+The Bodacious Bazaar is a 3 day shopping extravaganza and festival held twice a year, <br />
+in the
+<a style="text-decoration:none; color:#93C;" href="/fall">Fall</a> and
+<a style="text-decoration:none; color:#93C;" href="/spring">Spring</a>, located in
+Hampton, Virginia.
+
+<br />
+<br />
+We are a family owned and operated business. We are community minded, with a strong belief that you get back what you put out. Our goal is to bring a quality show to the Hampton Roads Area and to donate a portion of the proceeds to chosen local nonprofits.
+
+</div>
+<br />
+</center>
+<div style="position:right bottom; margin: auto 20px 20px auto; text-align:right; font-family:Arial, Helvetica, sans-serif; font-size:11px; text-align:center">©2011-2015 The Bodacious Bazaar All Rights Reserved.</div>
+
+<!--<a href="http://www.fmauthority.com/"><img src="spring/images/fort-logo.gif" style=" width:15%; max-width:130px; z-index:-1; position:fixed; bottom:15px; right:15px; border:0;"/></a>
+-->
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-3201046-5']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</body>
+</html>
---
+++ public_html/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta content="The Bodacious Bazaar is a 3 day shopping extravaganza and festival held twice a yearin the Spring and Fall located in Hampton Virginia. We are a family owned and operated business and community minded. Our goal is to bring a quality show to the Hampton Roads Area and to donate a portion of the proceeds to chosen local nonprofits." name="Description" />
+	<meta content="Bodacious Bazaar Shopping Fall Bazaar Shopping Spring Gifts Hampton Roads Festival" name="Keywords" />
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K37Q698');</script>
+<!-- End Google Tag Manager -->
+	
+	<title>Bodacious Bazaar</title>
+
+</head>
+
+<body onload="window.location = 'https://www.bodaciousbazaar.com/fall/canceled.html'">
+	
+	<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K37Q698"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+	
+<center>
+<img class="logo" itemprop="image" src="images/bobaz-logo-text.png" alt="Bodacious Bazaar" title="Bodacious Bazaar" /><br />
+	<p style="font-family:'Lobster', cursive; text-align: center; font-size: 60px; color: #FF0000">Welcome</p>
+	<p style="font-family:'Lobster', cursive; text-align: center; font-size: 60px; color: #FF0000"><img src="images/BoBaz-logo-BOTH.png" style=" width: 50%; max-width: 500px;" alt=""/></p>
+<center><div style="text-decoration:none; color:#333; font-family:Georgia 'Times New Roman' Times serif; font-size:16px; max-width: 800px; width: 90%; margin: 0 auto;">
+The Bodacious Bazaar is a 3 day shopping extravaganza and festival held twice a year <br />
+in the
+<a style="text-decoration:none; color:#93C;" href="/fall">Fall</a> and
+<a style="text-decoration:none; color:#93C;" href="/spring">Spring</a> located in
+Hampton Virginia.
+
+<br />
+<br />
+We are a family owned and operated business. We are community minded with a strong belief that you get back what you put out. Our goal is to bring a quality show to the Hampton Roads Area and to donate a portion of the proceeds to chosen local nonprofits.
+
+
+</div>
+<br />
+</center>
+	<hr />
+<div style="position:right bottom; margin: auto 20px 20px auto; text-align:right; font-family:Arial Helvetica sans-serif; font-size:11px; text-align:center">©2011-2019 The Bodacious Bazaar All Rights Reserved.</div>
+
+<!--<a href="http://www.fmauthority.com/"><img src="spring/images/fort-logo.gif" style=" width:15%; max-width:130px; z-index:-1; position:fixed; bottom:15px; right:15px; border:0;"/></a>
+-->
+
+ 
+
+
+</body>
+</html>
